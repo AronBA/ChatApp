ae4654846e78fc1d4378f7ba547c274047ac0a9b
Kenj-i <raphael.gisler@stud.edubs.ch>
2022-09-09 11:30:21 +0200

    Created a JFrame and added settings to it

--- src/Gui.java
+++ src/Gui.java
@@ -1,2 +1,16 @@
-public class Gui {
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui extends JFrame {
+    Panel panel = new Panel();
+
+    Gui() {
+        this.setVisible(true);
+        this.setSize(500, 500);
+        this.setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        new Gui();
+    }
 }
