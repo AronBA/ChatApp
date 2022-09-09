09aef40b7047e7f4843d95df39cbdd2ad6379ef7
Kenj-i <raphael.gisler@stud.edubs.ch>
2022-09-09 12:17:40 +0200

    Added new Elements to GUI You can now submit an IP

--- src/Gui.java
+++ src/Gui.java
@@ -1,15 +1,56 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-public class Gui extends JFrame {
+public class Gui extends JFrame implements ActionListener {
+    private int ip;
+    GridLayout gridLayout = new GridLayout(0, 1);
     Panel panel = new Panel();
+    Panel theirPanel = new Panel();
+    Panel yourPanel = new Panel();
+    JSpinner inputIp = new JSpinner();
+    JButton connectButton = new JButton("Connect");
+    JTextField yourIp = new JTextField();
 
     Gui() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
+        connectButton.addActionListener(this);
+        //try {
+        //    yourIp.setText(String.valueOf(InetAddress.getLocalHost()));
+        //} catch (Exception ex) {
+        //    ex.printStackTrace();
+        //}
+        Dimension size = new Dimension(100, 30);
+        inputIp.setPreferredSize(size);
+        yourIp.setPreferredSize(size);
+        theirPanel.add(new JLabel("Your friends Ip:"));
+        theirPanel.add(inputIp);
+        theirPanel.add(connectButton);
+        yourPanel.add(yourIp);
+        panel.setLayout(gridLayout);
+        panel.add(theirPanel);
+        panel.add(yourPanel);
+        this.add(panel);
     }
 
+    public int getIp() {
+        return ip;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == connectButton) {
+            int ip = (int) this.inputIp.getValue();
+            this.ip = ip;
+        }
+    }
     public static void main(String[] args) {
         new Gui();
     }
