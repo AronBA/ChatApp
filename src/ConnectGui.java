f5f87011185495e134b4e8b7a9edf38dc0163e7d
Kenj-i <raphael.gisler@stud.edubs.ch>
2022-09-12 11:10:21 +0200

    Own IP can be copied to clipboard

--- src/ConnectGui.java
+++ src/ConnectGui.java
@@ -1,12 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
-public class Gui extends JFrame implements ActionListener {
+public class ConnectGui extends JFrame implements ActionListener {
     private int ip;
     GridLayout gridLayout = new GridLayout(0, 1);
     Panel panel = new Panel();
@@ -15,18 +15,20 @@ public class Gui extends JFrame implements ActionListener {
     JSpinner inputIp = new JSpinner();
     JButton connectButton = new JButton("Connect");
     JTextField yourIp = new JTextField();
+    JButton copyIp = new JButton("Copy");
 
-    Gui() {
+    ConnectGui() {
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setVisible(true);
         this.setSize(500, 500);
         this.setLocationRelativeTo(null);
         connectButton.addActionListener(this);
-        //try {
-        //    yourIp.setText(String.valueOf(InetAddress.getLocalHost()));
-        //} catch (Exception ex) {
-        //    ex.printStackTrace();
-        //}
+        copyIp.addActionListener(this);
+        try {
+            System.out.println(Inet4Address.getLocalHost().getHostAddress());
+            yourIp.setText("172.31.34.206");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
         Dimension size = new Dimension(100, 30);
         inputIp.setPreferredSize(size);
         yourIp.setPreferredSize(size);
@@ -34,10 +36,12 @@ public class Gui extends JFrame implements ActionListener {
         theirPanel.add(inputIp);
         theirPanel.add(connectButton);
         yourPanel.add(yourIp);
+        yourPanel.add(copyIp);
         panel.setLayout(gridLayout);
         panel.add(theirPanel);
         panel.add(yourPanel);
         this.add(panel);
+        this.setVisible(true);
     }
 
     public int getIp() {
@@ -47,11 +51,16 @@ public class Gui extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == connectButton) {
-            int ip = (int) this.inputIp.getValue();
-            this.ip = ip;
+            this.ip = (int) this.inputIp.getValue();
+        }
+        if (e.getSource() == copyIp) {
+            String ownIp = yourIp.getText();
+            StringSelection stringSelection = new StringSelection(ownIp);
+            Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+            clipboard.setContents(stringSelection, null);
         }
     }
     public static void main(String[] args) {
-        new Gui();
+        new ConnectGui();
     }
 }
