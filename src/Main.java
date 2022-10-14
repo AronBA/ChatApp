7bc2be18592c7823a8925196618759f788ff9c41
AronBA <72823328+AronBA@users.noreply.github.com>
2022-10-14 19:46:52 +0200

    added laf

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        com.formdev.flatlaf.intellijthemes.FlatNordIJTheme.setup();
+
         ConnectGui c1 = new ConnectGui();
-
-        Client client = new Client(c1.getIp(),5000);
         MainGui g1 = new MainGui();
-
         Host host = new Host(5000,g1);
+        Client client = new Client(c1.getIp(),5000);
+
+
+
 
 
     }
